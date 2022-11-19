dec4926844f7c20fb1ea2cc0e2b61511156893a7
Hoàng Minh Tú <87486387+minhtu2k6bg@users.noreply.github.com>
2022-11-19 22:50:58 +0700

    Create repl.cpp

---
+++ repl.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s,s1,s2;
+int dem=0;
+int main()
+{
+  getline(cin,s);
+  getline(cin,s1);
+  getline(cin,s2);
+  while (s.size(s1) != string::npos
+         { 
+           int k= s.find(s1);
+           s.replace(k,s1.size(),s2);
+         }
+         cout << s;
+         return 0;
+         }
